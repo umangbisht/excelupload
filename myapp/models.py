5dded43c8029d75790d76011ee296b57efc0fece
umangbisht <umangbisht77@gmail.com>
2020-06-23 22:51:31 +0530

    second commit

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
+
+class ExcelData(models.Model):
+    all_record  =   models.CharField(max_length=255, null=True, blank=True)
+    file_name   =   models.CharField(max_length=255, null=True, blank=True)
+    created_at  =   models.DateTimeField(default=timezone.now)
+    updated_at  =   models.DateTimeField(default=timezone.now)
+    class Meta:
+        db_table = "excel_data"
